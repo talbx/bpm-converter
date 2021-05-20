c67a6242ca3b688123a8d1b233ea0f08ff37596d
talbx <dev@talbx.de>
2021-05-20 10:35:47 +0200

    feat: display version in header

--- src/header.component.tsx
+++ src/header.component.tsx
@@ -1,11 +1,18 @@
 import React from "react";
-import {Header, Icon} from "semantic-ui-react";
+import {Container, Header, Icon, Segment} from "semantic-ui-react";
+
+const pjson = require('./../package.json');
 
 export const HeaderComponent = () => {
     return (
-        <Header style={{"margin": "1%"}} as='h2'>
-            <Icon name='exchange' />
-            <Header.Content>BPM Converter</Header.Content>
-        </Header>
-    )
+        <Segment basic>
+            <Header as='h2' floated={"left"} textAlign='left'>
+                <Icon name='exchange'/>
+                BPM Converter
+            </Header>
+            <Header floated={"right"} as='h5' textAlign='right'>
+                v{pjson.version}
+            </Header>
+        </Segment>
+    );
 }
